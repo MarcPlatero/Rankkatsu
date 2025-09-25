1b37df069178a337ce046e3fbf83bf7f103546fb
MarcPlateroPolitecnics <170956509+MarcPlatero@users.noreply.github.com>
2025-09-26 00:51:22 +0200

    Fent proves inicials

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -6,6 +6,13 @@
 
         <title>{{ config('app.name', 'Laravel') }}</title>
 
+        <!-- Afegit de prova -->
+        <body>
+            <div id="app"></div>
+            @vite('resources/js/app.js')
+        </body>
+
+
         <!-- Fonts -->
         <link rel="preconnect" href="https://fonts.bunny.net">
         <link href="https://fonts.bunny.net/css?family=instrument-sans:400,500,600" rel="stylesheet" />
