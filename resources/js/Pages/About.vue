e371612fa31484ffacd455c7ab413655085b3c2b
MarcPlateroPolitecnics <170956509+MarcPlatero@users.noreply.github.com>
2025-09-26 23:27:22 +0200

    About

---
+++ resources/js/Pages/About.vue
@@ -0,0 +1,11 @@
+<script setup>
+</script>
+
+<template>
+  <div class="p-6">
+    <h1 class="text-2xl font-bold">Sobre aquest projecte</h1>
+    <p class="mt-2 text-gray-600">
+      Aquesta és la pàgina feta amb Vue + Inertia
+    </p>
+  </div>
+</template>
