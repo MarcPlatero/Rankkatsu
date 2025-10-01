7a0780ba5270a5da244a5cb9566a38780dc54709
MarcPlateroPolitecnics <170956509+MarcPlatero@users.noreply.github.com>
2025-10-01 20:27:55 +0200

    Create BackButton

---
+++ resources/js/Components/BackButton.vue
@@ -0,0 +1,9 @@
+<script setup>
+import { Link } from '@inertiajs/vue3'
+</script>
+
+<template>
+  <Link href="/" class="flex items-center text-blue-600 hover:text-blue-800 font-medium">
+    ← Tornar a Home
+  </Link>
+</template>
