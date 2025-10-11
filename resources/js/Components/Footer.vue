4212520c1fd8724ab45d9811e76481d573ce45c5
MarcPlateroPolitecnics <170956509+MarcPlatero@users.noreply.github.com>
2025-10-11 23:54:34 +0200

    Update Footer

--- resources/js/Components/Footer.vue
+++ resources/js/Components/Footer.vue
@@ -1,4 +1,3 @@
-<!-- resources/js/Components/Footer.vue -->
 <template>
   <footer class="bg-white border-t border-gray-200 py-6 mt-12">
     <div class="max-w-7xl mx-auto px-4 text-center text-gray-500">
