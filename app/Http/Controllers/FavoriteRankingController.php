1868903f8a858b572d700416bab8d3f90e3b2eae
MarcPlateroPolitecnics <170956509+MarcPlatero@users.noreply.github.com>
2025-12-05 23:48:14 +0100

    Update FavoriteRankingController

--- app/Http/Controllers/FavoriteRankingController.php
+++ app/Http/Controllers/FavoriteRankingController.php
@@ -8,27 +8,24 @@ use Illuminate\Support\Facades\Auth;
 
 class FavoriteRankingController extends Controller
 {
+    /**
+     * Gestiona l'acció d'afegir/treure favorits
+     */
     public function toggle(Request $request, Ranking $ranking)
     {
-        $user = Auth::user();
+        $user = $request->user();
 
+        // Si ja existeix, l'esborrem
         if ($user->favoriteRankings()->where('ranking_id', $ranking->id)->exists()) {
-            // Si ja és favorit → eliminar
             $user->favoriteRankings()->detach($ranking->id);
+            
             return back()->with('success', 'Eliminat dels favorits.');
-        } else {
-            // Si no és favorit → afegir
+        } 
+        // Si no existeix, l'afegim
+        else {
             $user->favoriteRankings()->attach($ranking->id);
+            
             return back()->with('success', 'Afegit als favorits!');
         }
-    }
-
-    public function check(Ranking $ranking)
-    {
-        $user = Auth::user();
-
-        $isFavorited = $user->favoriteRankings()->where('ranking_id', $ranking->id)->exists();
-
-        return response()->json(['favorited' => $isFavorited]);
     }
 }
