68a1cf6ab31d67c9fbd2cf4276445a8d2e36a3bc
MarcPlateroPolitecnics <170956509+MarcPlatero@users.noreply.github.com>
2025-12-14 01:36:31 +0100

    Update Register

--- resources/js/Pages/Auth/Register.vue
+++ resources/js/Pages/Auth/Register.vue
@@ -64,6 +64,7 @@ const submit = () => {
               required
               autofocus
               autocomplete="name"
+              maxlength="30"
             />
             <InputError class="mt-2" :message="form.errors.name" />
           </div>
