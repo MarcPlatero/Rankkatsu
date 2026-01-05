b71b9ce7161b4b6ac16d712143bd9ab1c73007ff
MarcPlateroPolitecnics <170956509+MarcPlatero@users.noreply.github.com>
2026-01-05 20:51:12 +0100

    Update Footer

--- resources/js/Components/Footer.vue
+++ resources/js/Components/Footer.vue
@@ -3,7 +3,7 @@
     class="bg-white dark:bg-gray-900 border-t border-gray-200 dark:border-gray-700 py-6 mt-12 transition-colors duration-300"
   >
     <div class="max-w-7xl mx-auto px-4 text-center text-gray-500 dark:text-gray-400">
-      © 2025 RankDuel. Tots els drets reservats.
+      © 2026 Rankkatsu. Tots els drets reservats.
     </div>
   </footer>
 </template>
