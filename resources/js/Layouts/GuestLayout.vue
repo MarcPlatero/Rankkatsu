91b4425bd877085062538764f7f5edeedef11481
MarcPlateroPolitecnics <170956509+MarcPlatero@users.noreply.github.com>
2026-01-28 00:38:00 +0100

    Rename Inputs

--- resources/js/Layouts/GuestLayout.vue
+++ resources/js/Layouts/GuestLayout.vue
@@ -1,5 +1,5 @@
 <script setup>
-import ApplicationLogo from '@/Components/ApplicationLogo.vue';
+import ApplicationLogo from '@/Components/LogoApplication.vue';
 import { Link } from '@inertiajs/vue3';
 </script>
 
